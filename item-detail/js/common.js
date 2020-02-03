c0e932a59c47a9947155a826950158a4de1eb814
KazukiYoza <yzkzk0323@gmail.com>
2020-02-03 16:44:46 +0900

    1st commit

---
+++ item-detail/js/common.js
@@ -0,0 +1,20 @@
+// $(function(){
+
+    $('.slider').slick({
+        autoplay:true,
+        autoplaySpeed:5000,
+        dots:true,
+    });
+
+    jQuery(function(){
+        $('.tab').click(function(){
+            $('.is-active').removeClass('is-active');
+            $(this).addClass('is-active');
+            $('.is-show').removeClass('is-show');
+            // クリックしたタブからインデックス番号を取得
+            const index = $(this).index();
+            // クリックしたタブと同じインデックス番号をもつコンテンツを表示
+            $('.panel').eq(index).addClass('is-show');
+        });
+    });
+// });
---
+++ js/common.js
@@ -0,0 +1,82 @@
+// PCサイドメニュー
+$(".sidebar > nav > ul > li").hover(function() {
+    //マウスカーソルが重なった時の処理
+    $(this).children('.nav-child').css('visibility', 'visible');
+},
+function() {
+    //マウスカーソルが離れた時の処理
+    $(this).children('.nav-child').css('visibility', 'hidden');
+});
+
+// 楽天検索ボックス
+function Form_Load() {
+    if (this.searchForm.sitem.value.length > 0) {
+      target = document.getElementById("clear_button");
+      target.style.visibility = "visible";
+    }
+  }
+
+// SPハンバーガーメニュー
+    $(function () {
+        $('#nav-toggle').on('click', function() {
+            $('body').toggleClass('open');
+        });
+        $('#navigation li').on({
+            'mouseenter' : function(){
+                var index = $(this).index();
+                $('#nav-bg-wrap .nav-bg').eq(index).addClass('active');
+                
+            },
+            'mouseleave' : function(){
+                var index = $(this).index();
+                $('#nav-bg-wrap .nav-bg').eq(index).removeClass('active');
+            }
+        });
+    });
+
+// SPカテゴリーアコーディオン
+$('.cat__menu__item__link').on('click',function(){
+    $(this).next('.cat__submenu').slideToggle(700);
+    $(this).find('.fa-plus').toggleClass('open');
+    return false;
+});
+
+
+
+	//★ ○○○_pcと○○○_spの画像を切り替える
+	// 置換の対象とするclass属性。
+	var $elem = $('.img-switch');
+	// 置換の対象とするsrc属性の末尾の文字列。
+	var sp = '_sp.jpg';
+	var pc = '.jpg';
+	// 画像を切り替えるウィンドウサイズ。
+	var replaceWidth = 1025;
+	function imageSwitch() {
+		// ウィンドウサイズを取得する。
+		// var windowWidth = parseInt(window.innerWidth);
+        var windowWidth = $(window).width();
+		// ページ内にあるすべての`.js-image-switch`に適応される。
+		$elem.each(function() {
+			var $this = $(this);
+			// ウィンドウサイズが1025px以上であれば_spを_pcに置換する。
+			if(windowWidth >= replaceWidth) {
+				$this.attr('src', $this.attr('src').replace(sp, pc));
+
+			// ウィンドウサイズが1025px未満であれば_pcを_spに置換する。
+			} else {
+				$this.attr('src', $this.attr('src').replace(pc, sp));
+			}
+			});
+	}
+	imageSwitch();
+
+$(function() {
+    var w = $(window).width();
+    var sp = 500;
+    var pc = 1024;
+    if (w <= x) {
+        $('#sample').css({
+            color: 'red'
+        });
+    }
+});
